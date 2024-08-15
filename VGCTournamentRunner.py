7c0dd3c8d5bfbe296546f472a1cde4bcd5c7afd7
Lance <SoccerLanceTobey@gmail.com>
2024-08-14 18:47:43 -0600

    Create VGCTournamentRunner.py

---
+++ VGCTournamentRunner.py
@@ -0,0 +1,15 @@
+
+
+# Gets the information of each player participating in the tournament, and enters them into the tournament.
+def enterPlayers():
+    playerList = []
+    print("Please enter a minimum of four players for the tournament.")
+    morePlayersNeeded = True
+    addingPlayers = True
+    print()
+    while (addingPlayers):
+        print("Please enter the name of player #" + str(len(playerList) + 1) + ": ")
+
+
+
+    return playerList
